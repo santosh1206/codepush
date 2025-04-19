0a0929bd7912feda22b006c44725c50fb36fa920
santosh sahoo <tukun1206@gmail.com>
2025-04-19 13:19:39 +0530

    rg name change

--- variables.tf
+++ variables.tf
@@ -6,6 +6,6 @@ variable "resource_group_location" {
 
 variable "resource_group_name_prefix" {
   type        = string
-  default     = "rg"
+  default     = "test"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
